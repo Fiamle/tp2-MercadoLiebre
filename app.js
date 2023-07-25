8640477d3ff4704d033a7a853ef9a57a0cc9fece
Macbook <fiammaledesma2000@gmail.com>
2023-07-25 20:05:49 -0300

    Nuevo servidor

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express()
 const path = require('path')
 app.use(express.static('public'))
 
-app.listen(3000,()=>
-console.log('Servidor Corriendo'))
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Servidor corriendo");
+})
 
 app.get('/',function(req,res){
 res.sendFile(path.join(__dirname,'/views/home.html'))
